d6184b3cdbbb32fda24a41c2d7a5096068c09bb5
Harshit Daga <78150691+harshitdaga7@users.noreply.github.com>
2021-11-28 18:54:03 +0530

    updated python script , added links.txt for all folders

--- link_generator.py
+++ link_generator.py
@@ -8,10 +8,13 @@ BASE_LINK = 'https://raw.githubusercontent.com/DSC-TCET/Assets/main/'
 
 #todo -> add target directory from where to generate the content links
 # example -> "image/events/"
-TARGET_DIR = "image/events/"
+# note end with "/"
+TARGET_DIRS= ["image/sponsors/","image/events/","image/team/"]
 
+#file names for which we dont want to generate link
+EXCLUDE_FILES = ['links.txt','readme.md']
 
-def generate():
+def generate(TARGET_DIR):
 
 	if not TARGET_DIR:
 		print('Please add TARGET_DIR and run again')
@@ -19,12 +22,18 @@ def generate():
 
 
 	content = os.listdir(TARGET_DIR)
+
+	for ex in EXCLUDE_FILES:
+		if ex in content:
+			content.remove(ex)
+
 	generated_links = [(BASE_LINK + TARGET_DIR + i + " \n") for i in content]
 	with open(TARGET_DIR + "links.txt", "w") as file1:
 		file1.writelines(generated_links)
 		print(f"added links.txt at {TARGET_DIR}")
 
-generate()
+for TARGET_DIR in TARGET_DIRS:
+	generate(TARGET_DIR)
 
 
 
